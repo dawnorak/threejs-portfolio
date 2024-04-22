fc6b170a99824524b12fd2d8bea0c70854605b50
dawnorak <sathi.vaigarai_2024@woxsen.edu.in>
2024-04-22 21:28:05 +0530

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
                 <p className="blue-gradient_text">DAWN</p>
             </NavLink>
             <nav className="flex text-lg gap-7 font-medium">
-                <NavLink to="/about" className={( { isActive }) => isActive ? "text-blue-500" : "text-black"}>
+                <NavLink to="/portfolio/about" className={( { isActive }) => isActive ? "text-blue-500" : "text-black"}>
                     About
                 </NavLink>
-                <NavLink to="/projects" className={( { isActive }) => isActive ? "text-blue-500" : "text-black"}>
+                <NavLink to="/portfolio/projects" className={( { isActive }) => isActive ? "text-blue-500" : "text-black"}>
                     Projects
                 </NavLink>
             </nav>
